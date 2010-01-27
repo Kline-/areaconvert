d6beab858fda366b6141d6b0da4e6c9a47aacd6a
Matthew Goff <matt@goff.cc>
2010-01-27 07:48:23 +0000

    Back-filling the 1.0.0 release.

---
+++ h/flags.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright Matt Goff (Kline) 2009
+ * If you use my code, please give credit where it is due.
+ * Support provided at www.ackmud.net
+ */
+
+#include "include.h"
+
+void flag_handler         ( int typein, int typeout );
+void clear_area_flag      ( string name );
+
+void flags_ack431_ackfuss ( void );
+void aflag_ack431_ackfuss ( void );
+void rflag_ack431_ackfuss ( void );
+void nflag_ack431_ackfuss ( void );
+void oflag_ack431_ackfuss ( void );
